a0c7458696acb113624ebefae85b6129ade4ce14
Vitali <31886478+l3m0nqu1z@users.noreply.github.com>
2021-03-10 17:45:54 +0300

    Update git.sh

--- git.sh
+++ git.sh
@@ -49,7 +49,7 @@ i=0
 while kill -0 $pid 2>/dev/null
 do
 	i=$(( (i+1) %4 ))
-  printf "\r[${spin:$i:1}] Please wait. Loading repo's data... "
+	printf "\r[${spin:$i:1}] Please wait. Loading repo's data... "
   sleep .1
 done
 }
